b95b8dee2c1a194dd654667eac5a9f09aa854701
viniciussdg1 <93990035+viniciussdg1@users.noreply.github.com>
2024-04-22 19:16:31 -0300

    Add files via upload

--- src/screens/placamaes/placamaes.tsx
+++ src/screens/placamaes/placamaes.tsx
@@ -1,9 +1,12 @@
 import { RouteProp, useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { StyleSheet, Text, View, ScrollView} from "react-native";
+import { StyleSheet, Text, View, ScrollView, ToastAndroid, TextInput, Button, Alert} from "react-native";
 import { Image } from 'react-native';
 import Produtos from "../../components/componenttest";
 import { NavegacaoParams } from "../../navigations/categorias";
+import { Modalize } from "react-native-modalize";
+import { useRef } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 
 export interface ScreenProps {
@@ -14,6 +17,23 @@ export default function Placamaes(props: any) {
 
     type navProp = StackNavigationProp<NavegacaoParams, "Placamaes">;
     const navigation = useNavigation<navProp>();
+
+    const modal = useRef<Modalize>();
+
+    const confirmaCompra = () => {
+      ToastAndroid.show('Compra realizada', ToastAndroid.LONG);
+      modal.current?.close();
+    }
+
+
+    const abrir = () => {
+      try {
+        modal.current?.open();
+      } catch (erro) {
+        console.log(erro)
+      }
+    }
+
 
 
 
@@ -28,24 +48,39 @@ export default function Placamaes(props: any) {
         <Text style={styles.text}>Placas Mãe</Text>
 
         <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-          <Produtos img={require('../../assets/imgs/RTX3080.jpg')} cost="R$4.140,90" onClick={()=> console.log('Clicou')}>
-            RTX 3080
+          <Produtos img={require('../../assets/imgs/tuf.jpg')} cost="R$940,90" onClick={abrir}>
+            Asus TUF B660
           </Produtos>
-          <Produtos img={require('../../assets/imgs/RX6800.jpg')} cost="R$3.880,90" onClick={()=> console.log('Clicou') }>
-            RX 6800
-          </Produtos>
-        </View>
-
-        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-          <Produtos img={require('../../assets/imgs/RX6700xt.jpg')} cost="R$3.120,90" onClick={()=> alert('CLICOU')}>
-            RX 6700 xt
-          </Produtos>
-          <Produtos img={require('../../assets/imgs/rxt3060ti.jpg')} cost="R$2800,00" onClick={()=> alert('CLICOU')}>
-            RTX 3060ti
+          <Produtos img={require('../../assets/imgs/aorus.jpg')} cost="R$1.880,90" onClick={abrir}>
+            Aorus B660
           </Produtos>
         </View>
-
       </ScrollView>
+      <GestureHandlerRootView style={styles.container}>
+
+      <Modalize 
+        ref={modal}
+        modalHeight={200}
+      >
+        <View style={{padding: 10}}>
+          <Text>Compra de Produto</Text>
+          <TextInput placeholder='Digite seu endereço'/>
+          <View style={{flexDirection: 'row'}}>
+            <Button title="BOLETO" onPress={confirmaCompra}/>
+            <Button title="PIX"  onPress={confirmaCompra}/>
+            <Button title="CARTÃO"  onPress={confirmaCompra}/>
+          </View>
+
+          <Button title="Cancelar" color="tomato" onPress={() => {
+            Alert.alert('Cancelar', 'Deseja realmente cancelar a compra?', [
+              { text: 'Sim', onPress: ()=> modal.current?.close()},
+              { text: 'Não'}
+            ])  
+            
+          }} />
+        </View>
+      </Modalize>
+    </GestureHandlerRootView>
         </>
     )
 }
@@ -68,7 +103,6 @@ const styles = StyleSheet.create({
     },
     text:{
         alignItems: 'center',
-        fontFamily: 'Anton_400Regular',
         fontSize: 26,
         marginHorizontal: '1%'
     },
